a6fe7b9ed39fedca8a7739384819ddf9bcbf0673
acorn <acorn@acorn-PC>
2017-09-01 17:58:31 +0900

    branch 테스트

--- SampleTest/src/Test.java
+++ SampleTest/src/Test.java
@@ -3,12 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
-		System.out.println("World");
-		System.out.println("Happy");
-		System.out.println("ehlsmswl ghkrdls");
-		System.out.println("dhoksehladskf");
-		System.out.println("마지막엔 push");
+		System.out.println("이것은 브랜치 테스트");
 	}
 
 }
